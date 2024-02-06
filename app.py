14053e7f720196c89ec260461fd4ad3bd5fa4aa7
YYTLZXD2327 <yytlzxd@yytlzxd2327.top>
2024-02-06 20:33:54 +0800

    ‘first’

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, send_from_directory
+import os
+import math
+from datetime import datetime
+
+app = Flask(__name__)
+
+download_folder = 'download'
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0 B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2) if i > 0 else size_bytes
+    return "%s %s" % (s, size_name[i])
+
+def get_file_info(file_path):
+    file_name = os.path.basename(file_path)
+    file_size = os.path.getsize(file_path)
+    file_size = convert_size(file_size)
+    file_last_modified = datetime.fromtimestamp(os.path.getmtime(file_path))
+    return (file_name, file_size, file_last_modified)
+@app.route('/')
+def index():
+    files_info = []
+
+    for file_name in os.listdir(download_folder):
+        file_path = os.path.join(download_folder, file_name)
+        if os.path.isfile(file_path):
+            file_info = get_file_info(file_path)
+            files_info.append(file_info)
+
+    # 根据文件的修改时间对文件信息进行排序
+    sorted_files_info = sorted(files_info, key=lambda x: x[2], reverse=True)
+
+    return render_template('index.html', files_info=sorted_files_info)
+
+@app.route('/download/<path:filename>')
+def download(filename):
+    return send_from_directory(download_folder, filename)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run()
